d6222deb0e9b8d079d0482b364356f5b91427b36
ciuser <ciuser@dxatscale.io>
2023-09-01 00:31:14 +0000

    feat: add support for multiple features

--- fetchAllEnvs/fetchAllEnvs.sh
+++ fetchAllEnvs/fetchAllEnvs.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 envs=""
 qa=""
 prod=""
-
 
 if [[ -z "$2" ]]; then
   echo >&2 "Fetch All Envs as filter was not provided"
@@ -11,34 +12,42 @@ if [[ -z "$2" ]]; then
       if [[ "$lower_env" == "qa" ]]; then
         qa="qa,"
       elif [[ "$lower_env" == "prod" || "$lower_env" == "production" ]]; then
-        prod="prod"
+        prod="$lower_env"
       else
         envs+="$lower_env,"
       fi
-    done
+  done
 else
-
-    filter_type=$(echo "$2" | cut -d':' -f1 | tr '[:lower:]' '[:upper:]')
-    filter_value=$(echo "$2" | cut -d':' -f2 | tr '[:upper:]' '[:lower:]')
-
-    for env in $(gh api -H "Accept: application/vnd.github+json" /repos/$1/environments | jq -r '.environments[] | .name'); do
-    type_value=$(gh api -H "Accept: application/vnd.github+json" /repos/$1/environments/$env/variables | jq -r --arg filter "$filter_type" '.variables[] | select(.name==$filter) | .value')
-    type_value=$(echo "$type_value" | tr '[:upper:]' '[:lower:]')
+  IFS=',' read -ra filters <<< "$2"  # Split the filters by comma into an array
+  
+  for env in $(gh api -H "Accept: application/vnd.github+json" /repos/$1/environments | jq -r '.environments[] | .name'); do
+    all_filters_match=true  # Assume all filters match initially
     
-    if [[ "$type_value" == "$filter_value" || -z "$2" ]]; then
-
-        lower_case_env=$(echo "$env" | tr '[:upper:]' '[:lower:]')
-        if [[ "$lower_case_env" == "qa" ]]; then
-            qa="qa,"
-          elif [[ "$lower_case_env" == "prod" || "$lower_case_env" == "production"  ]]; then
-            prod="prod"
-          else
-            envs+="$lower_case_env,"
-          fi
-    fi
+    for filter in "${filters[@]}"; do
+      filter_type=$(echo "$filter" | cut -d':' -f1 | tr '[:lower:]' '[:upper:]')
+      filter_value=$(echo "$filter" | cut -d':' -f2 | tr '[:upper:]' '[:lower:]')
+      
+      type_value=$(gh api -H "Accept: application/vnd.github+json" /repos/$1/environments/$env/variables | jq -r --arg filter "$filter_type" '.variables[] | select(.name==$filter) | .value')
+      type_value=$(echo "$type_value" | tr '[:upper:]' '[:lower:]')
+      
+      if [[ "$type_value" != "$filter_value" ]]; then
+        all_filters_match=false  # If one filter fails, set flag to false
+        break
+      fi
     done
-
- 
+    
+    if [ "$all_filters_match" = true ]; then
+      lower_case_env=$(echo "$env" | tr '[:upper:]' '[:lower:]')
+      if [[ "$lower_case_env" == "qa" ]]; then
+        qa="qa,"
+      elif [[ "$lower_case_env" == "prod" || "$lower_case_env" == "production" ]]; then
+        prod="prod"
+      else
+        envs+="$lower_case_env,"
+      fi
+    fi
+  done
+  
 fi
 
 envs="${qa}${envs}${prod}"
