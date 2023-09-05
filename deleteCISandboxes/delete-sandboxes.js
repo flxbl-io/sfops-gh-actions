a5a17754770a163e41d78e4101028de4d470c657
ciuser <ciuser@dxatscale.io>
2023-09-05 04:56:23 +0000

    fix: move to sf

--- deleteCISandboxes/delete-sandboxes.js
+++ deleteCISandboxes/delete-sandboxes.js
@@ -5,18 +5,18 @@ const { execSync } = require('child_process');
 function deleteSandbox(devHubUserName, sandboxName) {
     try {
         // Do a logout of this sandbox for resume to work
-        let stdOut = execSync(`sfdx org logout -o ${devHubUserName}.${sandboxName} -p 2>/dev/null`).toString();
+        let stdOut = execSync(`sf org logout -o ${devHubUserName}.${sandboxName} -p 2>/dev/null`).toString();
         console.log(stdOut);
     } catch (err) {
         console.log(`Sandbox not available for logout: ${sandboxName}`);
     }
 
     // Force sandbox to be resumed due to https://github.com/forcedotcom/cli/issues/1718
-    stdOut = execSync(`sfdx org resume sandbox -n ${sandboxName} -o ${devHubUserName}`).toString();
+    stdOut = execSync(`sf org resume sandbox -n ${sandboxName} -o ${devHubUserName}`).toString();
     console.log(stdOut);
 
     // Force sandbox to be deleted
-    stdOut = execSync(`sfdx org delete sandbox -o ${devHubUserName}.${sandboxName} -p`).toString();
+    stdOut = execSync(`sf org delete sandbox -o ${devHubUserName}.${sandboxName} -p`).toString();
     console.log(stdOut);
 
     console.log(`Successfully deleted sandbox: ${sandboxName}`);
@@ -58,7 +58,7 @@ function extractCompletedNumericSandboxNames(jsonData) {
 let repository = process.argv[2];
 let devHubUserName = process.argv[3];
 
-let stdOut = execSync(`sfdx data soql query -q "SELECT Id, Status, SandboxName, SandboxInfoId, LicenseType, CreatedDate, CopyProgress, SandboxOrganization, SourceId, Description, EndDate FROM SandboxProcess WHERE Status!='D'" -o ${devHubUserName} -t --json  2>/dev/null`).toString();
+let stdOut = execSync(`sf data query -q "SELECT Id, Status, SandboxName, SandboxInfoId, LicenseType, CreatedDate, CopyProgress, SandboxOrganization, SourceId, Description, EndDate FROM SandboxProcess WHERE Status!='D'" -o ${devHubUserName} -t --json  2>/dev/null`).toString();
 let jsonData = JSON.parse(stdOut);
 const completedNumericSandboxNames = extractCompletedNumericSandboxNames(jsonData);
 console.log(`Found ${completedNumericSandboxNames.length} completed sandboxes that matches spec`);
