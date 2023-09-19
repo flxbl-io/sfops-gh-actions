102838fc5dc5a326b659487727d76ec8135c2fe3
ciuser <ciuser@dxatscale.io>
2023-09-19 10:56:51 +0000

    fix: return first inUse sandbox before timing out

--- fetchCISandbox/fetchSandbox.sh
+++ fetchCISandbox/fetchSandbox.sh
@@ -1,25 +1,34 @@
 # Argument 1: GitHub Repository
 # Argument 2: Domain
 # Argument 3: Timeout in minutes
+# Argument 4: (Optional) --exit-on-timeout
+
 
 GITHUB_REPO=$1
 DOMAIN=$2
 TIMEOUT_MINUTES=$3
+EXIT_ON_TIMEOUT=$4
 
 DOMAIN=$(echo $DOMAIN | tr 'a-z' 'A-Z')
-end=$(date +%s)
 end=$(expr $(date +%s) + $((TIMEOUT_MINUTES * 60)))
 
 while true; do
   tempfile=$(mktemp)
   gh api /repos/$GITHUB_REPO/actions/variables?per_page=100 --jq ".variables[] | select(.name | test(\"^${DOMAIN}_.*_SBX\$\")).name" > "$tempfile"
 
+  firstInUseSandbox=""
 
   while read sandbox_name; do
     sandbox_json=$(gh api /repos/$GITHUB_REPO/actions/variables/$sandbox_name --jq ".value | fromjson")
     >&2 echo $sandbox_json
     status=$(echo $sandbox_json | jq -r '.status')
     isActive=$(echo $sandbox_json | jq -r '.isActive')
+
+    if [ -z "$firstInUseSandbox" ] && [ "$status" = "InUse" ]; then
+      firstInUseSandbox=$(echo $sandbox_json | jq -r '.name')
+    fi
+
+
     if [ "$status" = "Available" ] && [ "$isActive" = "true" ]; then
       availableSandbox=$(echo $sandbox_json | jq -r '.name')
       break
@@ -39,8 +48,14 @@ while true; do
   else
     current=$(date +%s)
     if [ $current -ge $end ]; then
-      >&2 echo "No available sandboxes found within $TIMEOUT_MINUTES minutes. Exiting."
-      exit 1
+     if [ "$EXIT_ON_TIMEOUT" = "--exit-on-timeout" ]; then
+        >&2 echo "No available sandboxes found within $TIMEOUT_MINUTES minutes. Exiting."
+        exit 1
+      else
+        >&2 echo "Returning first InUse sandbox: $firstInUseSandbox"
+        echo $firstInUseSandbox
+        exit 0
+      fi
     fi
 
     >&2 echo "No available sandboxes. Waiting for 30 seconds before checking again..."
