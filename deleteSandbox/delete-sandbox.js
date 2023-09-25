5c44a6c3d8d2a92f59efcb0005a07f33a5648bfb
ciuser <ciuser@dxatscale.io>
2023-09-25 10:43:54 +0000

    fix: add error handling to delete

--- deleteSandbox/delete-sandbox.js
+++ deleteSandbox/delete-sandbox.js
@@ -3,20 +3,33 @@ const { execSync } = require('child_process');
 
 
 function deleteSandbox(devHubUserName, sandboxName) {
+
+    console.log(`Attempting to logout of the sandbox if its already authenticated... `)
     try {
         // Do a logout of this sandbox for resume to work
-        let stdOut = execSync(`sfdx org logout -o ${devHubUserName}.${sandboxName} -p 2>/dev/null`).toString();
+        let stdOut = execSync(`sf org logout -o ${devHubUserName}.${sandboxName} -p 2>/dev/null`).toString();
         console.log(stdOut);
     } catch (err) {
         console.log(`Sandbox not available for logout: ${sandboxName}`);
     }
 
     // Force sandbox to be resumed due to https://github.com/forcedotcom/cli/issues/1718
-    stdOut = execSync(`sfdx org resume sandbox -n ${sandboxName} -o ${devHubUserName}`).toString();
-    console.log(stdOut);
+    console.log(`Attempting to authenticate to the sandbox`)
+    orgResumeJSONString = execSync(`sf org resume sandbox -n ${sandboxName} -o ${devHubUserName} --json`).toString();
+    orgResumeResult = JSON.parse(orgResumeJSONString);
+
+    if(orgResumeResult.status == 1)
+    {
+       if(orgResumeResult.name == 'SingleRecordQuery_NoRecords')
+       {
+        console.log(`The provided sandbox with name ${sandboxName} is no longer available in the org, Did you delete this directly?`);
+         return;
+       }
+    }
+
 
     // Force sandbox to be deleted
-    stdOut = execSync(`sfdx org delete sandbox -o ${devHubUserName}.${sandboxName} -p`).toString();
+    stdOut = execSync(`sf org delete sandbox -o ${devHubUserName}.${sandboxName} -p`).toString();
     console.log(stdOut);
 
     console.log(`Successfully deleted sandbox: ${sandboxName}`);
