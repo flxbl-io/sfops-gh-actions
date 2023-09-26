198f2d5d4b2af3a470bc52744d0d504826e9cf74
ciuser <ciuser@dxatscale.io>
2023-09-26 00:58:01 +0000

    fix: remove json due to incorrect behaviour from sf

--- deleteSandbox/delete-sandbox.js
+++ deleteSandbox/delete-sandbox.js
@@ -15,23 +15,15 @@ function deleteSandbox(devHubUserName, sandboxName) {
 
     // Force sandbox to be resumed due to https://github.com/forcedotcom/cli/issues/1718
     console.log(`Attempting to authenticate to the sandbox`)
-    try{
-            orgResumeJSONString = execSync(`sf org resume sandbox -n ${sandboxName} -o ${devHubUserName} --json`).toString();
-            orgResumeResult = JSON.parse(orgResumeJSONString);
-
-            if(orgResumeResult.status == 1)
-            {
-            if(orgResumeResult.name == 'SingleRecordQuery_NoRecords')
-            {
-                console.log(`The provided sandbox with name ${sandboxName} is no longer available in the org, Did you delete this directly?`);
-                return;
-            }
-          }
-    } catch(error)
-    {
-        console.log(`Error authenticating to sandbox: ${sandboxName} due to ${error}`);
-        console.log(`The provided sandbox with name ${sandboxName} is no longer available in the org, Did you delete this directly?`);
-        return;
+    try {
+        orgResumeJSONString = execSync(`sf org resume sandbox -n ${sandboxName} -o ${devHubUserName}`).toString();
+    } catch (error) {
+        if (error.message.includes(`No record found`)) {
+            console.log(`The provided sandbox with name ${sandboxName} is no longer available in the org, Did you delete this directly?`);
+            return 0;
+        }
+        else
+         throw error;
     }
 
 
