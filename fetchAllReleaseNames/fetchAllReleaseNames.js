92030c1961892812e19db15456ddc73d44b8ed79
ciuser <ciuser@dxatscale.io>
2023-10-05 02:30:12 +0000

    fix: copy release config while reporting packages

--- fetchAllReleaseNames/fetchAllReleaseNames.js
+++ fetchAllReleaseNames/fetchAllReleaseNames.js
@@ -4,20 +4,33 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
+function isYAML(fileContent) {
+  try {
+    yaml.load(fileContent);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
 
-
-
-
-function getAllReleaseDefns(configDir) {
+function getAllReleaseDefns(configDir, outputDir = null) {
   const allReleaseNames = [];
 
-  // First, collect all release names
   fs.readdirSync(configDir).forEach(file => {
-    if (file.startsWith('release-config')) {
-      const filePath = path.join(configDir, file);
-      const fileContent = fs.readFileSync(filePath, 'utf8');
+    const filePath = path.join(configDir, file);
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    
+    if (isYAML(fileContent)) {
       const releaseConfig = yaml.load(fileContent);
-      allReleaseNames.push(releaseConfig.releaseName); // adding every releaseName
+      if (releaseConfig.releaseName) {
+        allReleaseNames.push(releaseConfig.releaseName);
+        
+        if (outputDir) {
+          // Copy file to user-provided directory with renaming
+          const outputFilePath = path.join(outputDir, `${releaseConfig.releaseName}.yaml`);
+          fs.copyFileSync(filePath, outputFilePath);
+        }
+      }
     }
   });
 
@@ -32,14 +45,19 @@ function getAllReleaseDefns(configDir) {
 }
 
 function main() {
-  if (process.argv.length < 2) {
-    console.error('Usage: fetchAllReleaseNames.js <config_directory>');
+  if (process.argv.length < 3) {
+    console.error('Usage: fetchAllReleaseNames.js <config_directory> [output_directory]');
     process.exit(1);
   }
 
   const configDir = process.argv[2];
+  const outputDir = process.argv.length > 3 ? process.argv[3] : null;
 
-  let output = getAllReleaseDefns(configDir);
+  if (outputDir && !fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+  }
+
+  let output = getAllReleaseDefns(configDir, outputDir);
   console.error(`Found ${output.length} releases.`);
   console.log(output);
 }
