e729e16dd148522ee697f45529616f258f5ccd30
ciuser <ciuser@dxatscale.io>
2023-11-21 06:27:51 +0000

    feat: update package locks

---
+++ deploymentStatusUpdater/lib/execute.js
@@ -0,0 +1,238 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
+const core = __importStar(require("@actions/core"));
+const github = __importStar(require("@actions/github"));
+function listDeployments(client, { owner, repo, environment, ref = "" }, page = 0) {
+    return __awaiter(this, void 0, void 0, function* () {
+        core.debug(`Getting list of deployments in environment ${environment}`);
+        const { data } = yield client.request("GET /repos/{owner}/{repo}/deployments", {
+            owner,
+            repo,
+            environment,
+            ref,
+            per_page: 100,
+            page,
+        });
+        const deploymentRefs = data.map((deployment) => ({
+            deploymentId: deployment.id,
+            ref: deployment.ref,
+            description: deployment.description,
+        }));
+        core.debug(`Getting total of ${deploymentRefs.length} deployments on page ${page} `);
+        if (deploymentRefs.length === 100)
+            return deploymentRefs.concat(yield listDeployments(client, { owner, repo, environment, ref }, page + 1));
+        return deploymentRefs;
+    });
+}
+function setDeploymentInactive(client, { owner, repo, deploymentId }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.request("POST /repos/{owner}/{repo}/deployments/{deployment_id}/statuses", {
+            owner,
+            repo,
+            deployment_id: deploymentId,
+            state: "inactive",
+        });
+    });
+}
+function setDeploymentSucess(client, { owner, repo, deploymentId }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.request("POST /repos/{owner}/{repo}/deployments/{deployment_id}/statuses", {
+            owner,
+            repo,
+            deployment_id: deploymentId,
+            state: "success",
+        });
+    });
+}
+function setDeploymentFailure(client, { owner, repo, deploymentId }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.request("POST /repos/{owner}/{repo}/deployments/{deployment_id}/statuses", {
+            owner,
+            repo,
+            deployment_id: deploymentId,
+            state: "failure",
+        });
+    });
+}
+function deleteDeploymentById(client, { owner, repo, deploymentId }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.request("DELETE /repos/{owner}/{repo}/deployments/{deployment_id}", {
+            owner,
+            repo,
+            deployment_id: deploymentId,
+        });
+    });
+}
+function deleteTheEnvironment(client, environment, { owner, repo }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let existingEnv = false;
+        try {
+            const getEnvResult = yield client.request("GET /repos/{owner}/{repo}/environments/{environment_name}", {
+                owner,
+                repo,
+                environment_name: environment,
+            });
+            existingEnv = typeof getEnvResult === "object";
+        }
+        catch (err) {
+            if (err.status !== 404) {
+                core.error("Error deleting environment");
+                throw err;
+            }
+        }
+        if (existingEnv) {
+            core.info(`deleting environment ${environment}`);
+            yield client.request("DELETE /repos/{owner}/{repo}/environments/{environment_name}", {
+                owner,
+                repo,
+                environment_name: environment,
+            });
+            core.info(`environment ${environment} deleted`);
+        }
+    });
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let deleteDeployment = false;
+        let deleteEnvironment = false;
+        let markDeploymentAsSuccess = true;
+        let markDeploymentAsFailure = true;
+        const { context } = github;
+        const token = core.getInput("token", { required: true });
+        const environment = core.getInput("environment", { required: true });
+        const onlyRemoveDeployments = core.getInput("onlyRemoveDeployments", {
+            required: false,
+        });
+        const onlyDeactivateDeployments = core.getInput("onlyDeactivateDeployments", {
+            required: false,
+        });
+        const setDeploymentAsSuccess = core.getInput("setDeploymentAsSuccess", {
+            required: false,
+        });
+        const descriptionFilter = core.getInput("descriptionFilter", {
+            required: false,
+        });
+        const setDeploymentAsFailure = core.getInput("setDeploymentAsFailure", {
+            required: false,
+        });
+        const ref = core.getInput("ref", { required: false });
+        const client = github.getOctokit(token, {
+            throttle: {
+                onRateLimit: (retryAfter = 0, options) => {
+                    console.warn(`Request quota exhausted for request ${options.method} ${options.url}`);
+                    if (options.request.retryCount === 0) {
+                        // only retries once
+                        console.log(`Retrying after ${retryAfter} seconds!`);
+                        return true;
+                    }
+                },
+                onAbuseLimit: (retryAfter = 0, options) => {
+                    console.warn(`Abuse detected for request ${options.method} ${options.url}`);
+                    if (options.request.retryCount === 0) {
+                        // only retries once
+                        console.log(`Retrying after ${retryAfter} seconds!`);
+                        return true;
+                    }
+                },
+            },
+            previews: ["ant-man"],
+        });
+        if (onlyDeactivateDeployments === "true") {
+            deleteDeployment = false;
+            deleteEnvironment = false;
+        }
+        else if (onlyRemoveDeployments === "true") {
+            deleteEnvironment = false;
+        }
+        else if (setDeploymentAsSuccess === "true") {
+            deleteEnvironment = false;
+            deleteDeployment = false;
+            markDeploymentAsSuccess = true;
+            markDeploymentAsFailure = false;
+        }
+        else if (setDeploymentAsFailure === "true") {
+            deleteEnvironment = false;
+            deleteDeployment = false;
+            markDeploymentAsSuccess = false;
+            markDeploymentAsFailure = true;
+        }
+        core.debug(`Try to list deployments`);
+        try {
+            const deploymentRefs = yield listDeployments(client, Object.assign(Object.assign({}, context.repo), { environment,
+                ref }));
+            core.info(`Found ${deploymentRefs.length} deployments`);
+            let deploymentIds;
+            if (ref.length > 0 && descriptionFilter.length > 0) {
+                deploymentIds = deploymentRefs
+                    .filter((deployment) => {
+                    var _a;
+                    return deployment.ref === ref &&
+                        ((_a = deployment.description) === null || _a === void 0 ? void 0 : _a.includes(descriptionFilter));
+                })
+                    .map((deployment) => deployment.deploymentId);
+            }
+            if (ref.length > 0) {
+                deploymentIds = deploymentRefs
+                    .filter((deployment) => deployment.ref === ref)
+                    .map((deployment) => deployment.deploymentId);
+            }
+            else {
+                deploymentIds = deploymentRefs.map((deployment) => deployment.deploymentId);
+            }
+            if (markDeploymentAsFailure) {
+                yield Promise.all(deploymentIds.map((deploymentId) => setDeploymentFailure(client, Object.assign(Object.assign({}, context.repo), { deploymentId }))));
+            }
+            else if (markDeploymentAsSuccess) {
+                yield Promise.all(deploymentIds.map((deploymentId) => setDeploymentFailure(client, Object.assign(Object.assign({}, context.repo), { deploymentId }))));
+            }
+            if (deleteDeployment) {
+                yield Promise.all(deploymentIds.map((deploymentId) => setDeploymentInactive(client, Object.assign(Object.assign({}, context.repo), { deploymentId }))));
+                yield Promise.all(deploymentIds.map((deploymentId) => deleteDeploymentById(client, Object.assign(Object.assign({}, context.repo), { deploymentId }))));
+                core.info(`Deleted deployments`);
+            }
+            if (deleteEnvironment) {
+                yield Promise.all(deploymentIds.map((deploymentId) => setDeploymentInactive(client, Object.assign(Object.assign({}, context.repo), { deploymentId }))));
+                yield deleteTheEnvironment(client, environment, context.repo);
+            }
+            core.info("done");
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
+    });
+}
+exports.main = main;
+//# sourceMappingURL=execute.js.map
---
+++ deploymentStatusUpdater/src/execute.ts
@@ -0,0 +1,310 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { Octokit } from "@octokit/core";
+import { RequestError } from "@octokit/request-error";
+
+interface ListDeploymentIDs {
+  owner: string;
+  repo: string;
+  environment: string;
+  ref: string;
+}
+
+interface Deployment {
+  owner: string;
+  repo: string;
+  deploymentId: number;
+}
+
+interface Context {
+  owner: string;
+  repo: string;
+}
+
+export interface DeploymentRef {
+  deploymentId: number;
+  ref: string;
+  description: string | null;
+}
+
+async function listDeployments(
+  client: Octokit,
+  { owner, repo, environment, ref = "" }: ListDeploymentIDs,
+  page = 0
+): Promise<DeploymentRef[]> {
+  core.debug(`Getting list of deployments in environment ${environment}`);
+  const { data } = await client.request(
+    "GET /repos/{owner}/{repo}/deployments",
+    {
+      owner,
+      repo,
+      environment,
+      ref,
+      per_page: 100,
+      page,
+    }
+  );
+  const deploymentRefs: DeploymentRef[] = data.map((deployment) => ({
+    deploymentId: deployment.id,
+    ref: deployment.ref,
+    description: deployment.description,
+  }));
+  core.debug(
+    `Getting total of ${deploymentRefs.length} deployments on page ${page} `
+  );
+
+  if (deploymentRefs.length === 100)
+    return deploymentRefs.concat(
+      await listDeployments(client, { owner, repo, environment, ref }, page + 1)
+    );
+
+  return deploymentRefs;
+}
+
+async function setDeploymentInactive(
+  client: Octokit,
+  { owner, repo, deploymentId }: Deployment
+): Promise<void> {
+  await client.request(
+    "POST /repos/{owner}/{repo}/deployments/{deployment_id}/statuses",
+    {
+      owner,
+      repo,
+      deployment_id: deploymentId,
+      state: "inactive",
+    }
+  );
+}
+
+async function setDeploymentSucess(
+  client: Octokit,
+  { owner, repo, deploymentId }: Deployment
+): Promise<void> {
+  await client.request(
+    "POST /repos/{owner}/{repo}/deployments/{deployment_id}/statuses",
+    {
+      owner,
+      repo,
+      deployment_id: deploymentId,
+      state: "success",
+    }
+  );
+}
+
+async function setDeploymentFailure(
+  client: Octokit,
+  { owner, repo, deploymentId }: Deployment
+): Promise<void> {
+  await client.request(
+    "POST /repos/{owner}/{repo}/deployments/{deployment_id}/statuses",
+    {
+      owner,
+      repo,
+      deployment_id: deploymentId,
+      state: "failure",
+    }
+  );
+}
+
+async function deleteDeploymentById(
+  client: Octokit,
+  { owner, repo, deploymentId }: Deployment
+): Promise<void> {
+  await client.request(
+    "DELETE /repos/{owner}/{repo}/deployments/{deployment_id}",
+    {
+      owner,
+      repo,
+      deployment_id: deploymentId,
+    }
+  );
+}
+
+async function deleteTheEnvironment(
+  client: Octokit,
+  environment: string,
+  { owner, repo }: Context
+): Promise<void> {
+  let existingEnv = false;
+  try {
+    const getEnvResult = await client.request(
+      "GET /repos/{owner}/{repo}/environments/{environment_name}",
+      {
+        owner,
+        repo,
+        environment_name: environment,
+      }
+    );
+    existingEnv = typeof getEnvResult === "object";
+  } catch (err) {
+    if ((err as RequestError).status !== 404) {
+      core.error("Error deleting environment");
+      throw err;
+    }
+  }
+
+  if (existingEnv) {
+    core.info(`deleting environment ${environment}`);
+    await client.request(
+      "DELETE /repos/{owner}/{repo}/environments/{environment_name}",
+      {
+        owner,
+        repo,
+        environment_name: environment,
+      }
+    );
+    core.info(`environment ${environment} deleted`);
+  }
+}
+
+export async function main(): Promise<void> {
+  let deleteDeployment = false;
+  let deleteEnvironment = false;
+  let markDeploymentAsSuccess = true;
+  let markDeploymentAsFailure = true;
+
+  const { context } = github;
+  const token: string = core.getInput("token", { required: true });
+  const environment: string = core.getInput("environment", { required: true });
+  const onlyRemoveDeployments: string = core.getInput("onlyRemoveDeployments", {
+    required: false,
+  });
+  const onlyDeactivateDeployments: string = core.getInput(
+    "onlyDeactivateDeployments",
+    {
+      required: false,
+    }
+  );
+
+  const setDeploymentAsSuccess: string = core.getInput(
+    "setDeploymentAsSuccess",
+    {
+      required: false,
+    }
+  );
+  const descriptionFilter: string = core.getInput("descriptionFilter", {
+    required: false,
+  });
+  const setDeploymentAsFailure: string = core.getInput(
+    "setDeploymentAsFailure",
+    {
+      required: false,
+    }
+  );
+
+  const ref: string = core.getInput("ref", { required: false });
+
+  const client: Octokit = github.getOctokit(token, {
+    throttle: {
+      onRateLimit: (retryAfter = 0, options: any) => {
+        console.warn(
+          `Request quota exhausted for request ${options.method} ${options.url}`
+        );
+        if (options.request.retryCount === 0) {
+          // only retries once
+          console.log(`Retrying after ${retryAfter} seconds!`);
+          return true;
+        }
+      },
+      onAbuseLimit: (retryAfter = 0, options: any) => {
+        console.warn(
+          `Abuse detected for request ${options.method} ${options.url}`
+        );
+        if (options.request.retryCount === 0) {
+          // only retries once
+          console.log(`Retrying after ${retryAfter} seconds!`);
+          return true;
+        }
+      },
+    },
+    previews: ["ant-man"],
+  });
+
+  if (onlyDeactivateDeployments === "true") {
+    deleteDeployment = false;
+    deleteEnvironment = false;
+  } else if (onlyRemoveDeployments === "true") {
+    deleteEnvironment = false;
+  } else if (setDeploymentAsSuccess === "true") {
+    deleteEnvironment = false;
+    deleteDeployment = false;
+    markDeploymentAsSuccess = true;
+    markDeploymentAsFailure = false;
+  } else if (setDeploymentAsFailure === "true") {
+    deleteEnvironment = false;
+    deleteDeployment = false;
+    markDeploymentAsSuccess = false;
+    markDeploymentAsFailure = true;
+  }
+  core.debug(`Try to list deployments`);
+  try {
+    const deploymentRefs = await listDeployments(client, {
+      ...context.repo,
+      environment,
+      ref,
+    });
+    core.info(`Found ${deploymentRefs.length} deployments`);
+    let deploymentIds: number[];
+    if (ref.length > 0 && descriptionFilter.length > 0) {
+      deploymentIds = deploymentRefs
+        .filter(
+          (deployment) =>
+            deployment.ref === ref &&
+            deployment.description?.includes(descriptionFilter)
+        )
+        .map((deployment) => deployment.deploymentId);
+    }
+    if (ref.length > 0) {
+      deploymentIds = deploymentRefs
+        .filter((deployment) => deployment.ref === ref)
+        .map((deployment) => deployment.deploymentId);
+    } else {
+      deploymentIds = deploymentRefs.map(
+        (deployment) => deployment.deploymentId
+      );
+    }
+
+    if (markDeploymentAsFailure) {
+      await Promise.all(
+        deploymentIds.map((deploymentId) =>
+          setDeploymentFailure(client, { ...context.repo, deploymentId })
+        )
+      );
+    }
+    else if(markDeploymentAsSuccess)
+    {
+      await Promise.all(
+        deploymentIds.map((deploymentId) =>
+          setDeploymentFailure(client, { ...context.repo, deploymentId })
+        )
+      );
+    }
+    
+    if (deleteDeployment) {
+      await Promise.all(
+        deploymentIds.map((deploymentId) =>
+          setDeploymentInactive(client, { ...context.repo, deploymentId })
+        )
+      );
+      await Promise.all(
+        deploymentIds.map((deploymentId) =>
+          deleteDeploymentById(client, { ...context.repo, deploymentId })
+        )
+      );
+      core.info(`Deleted deployments`);
+    }
+
+    if (deleteEnvironment) {
+      await Promise.all(
+        deploymentIds.map((deploymentId) =>
+          setDeploymentInactive(client, { ...context.repo, deploymentId })
+        )
+      );
+      await deleteTheEnvironment(client, environment, context.repo);
+    }
+
+    core.info("done");
+  } catch (error) {
+    core.setFailed((error as RequestError).message);
+  }
+}
